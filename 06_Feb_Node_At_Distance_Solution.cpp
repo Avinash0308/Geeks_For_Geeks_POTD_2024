b702451c51da8f554905c812f6e209d24142f027
Avinash Agrawal <avinash03082003@gmail.com>
2024-02-06 01:28:04 +0530

    Added 06 Feb POTD

---
+++ 06_Feb_Node_At_Distance_Solution.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    Node *left, *right;
+    int key;
+    Node(int a)
+    {
+        key = a;
+        left = right = NULL;
+    }
+};
+class Solution
+{
+    int count = 0;
+    void solve(Node *root, int k, vector<int> &visit)
+    {
+        if (!root)
+            return;
+        visit.push_back(0);
+        if (!(root->left) && !(root->right))
+        {
+            if (k < visit.size() && visit[visit.size() - 1 - k] == 0)
+            {
+                visit[visit.size() - 1 - k] = 1;
+                count++;
+            }
+        }
+        solve(root->left, k, visit);
+        solve(root->right, k, visit);
+        visit.pop_back();
+        return;
+    }
+
+public:
+    // Function to return count of nodes at a given distance from leaf nodes.
+    int printKDistantfromLeaf(Node *root, int k)
+    {
+        // Add your code here.
+        vector<int> visit;
+        solve(root, k, visit);
+        return count;
+    }
+};
