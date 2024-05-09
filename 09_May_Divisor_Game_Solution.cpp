aea349245637945da34c4ea09f738d836cbd24d3
Avinash Agrawal <avinash03082003@gmail.com>
2024-05-09 15:17:04 +0530

    Added 09 May POTD

---
+++ 09_May_Divisor_Game_Solution.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    bool divisorGame(int n) {
+        // code here
+        return n%2 == 0;
+    }
+};
+
+int main(){
+    return 0;
+}
