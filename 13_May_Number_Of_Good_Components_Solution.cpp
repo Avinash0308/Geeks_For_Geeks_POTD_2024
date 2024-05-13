5cb8672440f6acb013c554a5a0d19e1eb6a9c8b5
Avinash Agrawal <avinash03082003@gmail.com>
2024-05-13 13:03:02 +0530

    Added 13 May POTD

---
+++ 13_May_Number_Of_Good_Components_Solution.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int solve(int ind, int n, vector<int> &visit, vector<vector<int>> &adj)
+    {
+        queue<int> q;
+        unordered_map<int, int> mp;
+        q.push(ind);
+        while (!q.empty())
+        {
+            int i = q.front();
+            q.pop();
+            visit[i] = 1;
+            for (int j = 1; j <= n; j++)
+            {
+                if (adj[i][j])
+                {
+                    mp[i]++;
+                    if (!visit[j])
+                    {
+                        visit[j] = 1;
+                        q.push(j);
+                    }
+                }
+            }
+        }
+        int x = int(mp.size()) - 1;
+        for (auto i : mp)
+        {
+            if (i.second != x)
+                return 0;
+        }
+        return 1;
+    }
+    int findNumberOfGoodComponent(int e, int v, vector<vector<int>> &edges)
+    {
+        // code here
+        vector<vector<int>> adj(v + 1, vector<int>(v + 1, 0));
+        for (auto edge : edges)
+        {
+            adj[edge[0]][edge[1]] = 1;
+            adj[edge[1]][edge[0]] = 1;
+        }
+        int ans = 0;
+        vector<int> visit(v + 1, 0);
+        for (int i = 1; i <= v; i++)
+        {
+            if (visit[i])
+                continue;
+            ans += solve(i, v, visit, adj);
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    return 0;
+}
