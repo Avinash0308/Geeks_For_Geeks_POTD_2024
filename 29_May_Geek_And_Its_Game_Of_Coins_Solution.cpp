37131dd987cd196e054c58d8004436f59417cc7b
Avinash Agrawal <avinash03082003@gmail.com>
2024-05-29 22:55:49 +0530

    Added 29 May POTD

---
+++ 29_May_Geek_And_Its_Game_Of_Coins_Solution.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int findWinner(int n, int x, int y)
+    {
+        if (x % 2 && y % 2)
+            return n % 2;
+        vector<bool> dp(n + 1, false);
+        for (int i = 1; i <= n; i++)
+        {
+            bool win = !dp[i - 1];
+            if (i >= x)
+                win |= !dp[i - x];
+            if (i >= y)
+                win |= !dp[i - y];
+            dp[i] = win;
+        }
+        return dp[n];
+    }
+};
+
+int main()
+{
+    return 0;
+}
