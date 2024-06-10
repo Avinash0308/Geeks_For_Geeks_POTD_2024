772c580cbdd45472e6a1753c9600f28347f5d745
Avinash Agrawal <avinash03082003@gmail.com>
2024-06-10 13:26:14 +0530

    Added 10 Jun POTD

---
+++ 10_Jun_Nuts_And_Bolts_Problem_Solution.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    void matchPairs(int n, char nuts[], char bolts[])
+    {
+        // code here
+        sort(nuts, nuts + n);
+        sort(bolts, bolts + n);
+    }
+};
+
+int main()
+{
+    return 0;
+}
