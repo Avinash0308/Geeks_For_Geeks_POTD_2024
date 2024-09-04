6d5f8227d65ca7a6b02ee64f877b37e5847625cd
Avinash Agrawal <avinash03082003@gmail.com>
2024-09-04 23:51:57 +0530

    Added 04 Sep POTD

---
+++ 04_Sep_Count_Ways_To_Nth_Stair_Order_Does_Not_Matter_Solution.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int nthStair(int n)
+    {
+        //  Code here
+        return (n / 2) + 1;
+    }
+};
+
+int main()
+{
+    return 0;
+}
