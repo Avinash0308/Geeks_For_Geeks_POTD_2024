295d7c5b24cd85016fffdc6b5f59826f1658fcd8
Avinash Agrawal <115834175+Avinash0308@users.noreply.github.com>
2024-10-07 19:38:36 +0530

    Added 07 Oct POTD

---
+++ 07_Oct_XOR_Linked_List_Solution.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *npx;
+
+    Node(int x)
+    {
+        data = x;
+        npx = NULL;
+    }
+};
+
+Utility function to get XOR of two Struct Node pointer
+    use this function to get XOR of two pointers struct Node *
+    XOR(struct Node *a, struct Node *b)
+{
+    return (struct Node *)((uintptr_t)(a) ^ (uintptr_t)(b));
+}
+* /
+
+    // function should insert the data to the front of the list
+    struct Node *insert(struct Node *head, int data)
+{
+    // Code here
+    Node *h = new Node(data);
+    h->npx = XOR(h, head);
+    if (head)
+        head->npx = XOR(head->npx, h);
+    head = h;
+    return head;
+}
+
+vector<int> getList(struct Node *head)
+{
+    // Code here
+    Node *prev = NULL;
+    vector<int> ans;
+    while (head != NULL)
+    {
+        ans.push_back(head->data);
+        Node *next = XOR(prev, head);
+        next = XOR(next, head->npx);
+        prev = head;
+        head = next;
+    }
+    return ans;
+}
+
+int main()
+{
+    return 0;
+}
