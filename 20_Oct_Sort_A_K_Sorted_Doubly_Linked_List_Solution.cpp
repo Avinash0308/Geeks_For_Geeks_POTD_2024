0ec97e48fb8c8b0f3d4e1f9ff5728761e5d303de
Avinash Agrawal <115834175+Avinash0308@users.noreply.github.com>
2024-10-20 11:30:15 +0530

    Added 20 Oct POTD

---
+++ 20_Oct_Sort_A_K_Sorted_Doubly_Linked_List_Solution.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class DLLNode
+{
+public:
+    int data;
+    DLLNode *next;
+    DLLNode *prev;
+    DLLNode(int val)
+    {
+        data = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+class Solution
+{
+public:
+    // function to sort a k sorted doubly linked list
+    DLLNode *sortAKSortedDLL(DLLNode *head, int k)
+    {
+
+        priority_queue<int, vector<int>, greater<int>> pq;
+
+        for (int i = 0; i <= k && head; i++)
+        {
+            pq.push(head->data);
+            head = head->next;
+        }
+
+        DLLNode *h = NULL;
+        DLLNode *t = NULL;
+
+        while (!pq.empty())
+        {
+            DLLNode *n = new DLLNode(pq.top());
+            pq.pop();
+
+            if (h == NULL)
+            {
+                h = n;
+                t = n;
+            }
+            else
+            {
+                DLLNode *temp = t;
+                t->next = n;
+                t = n;
+                t->prev = temp;
+            }
+            if (head)
+            {
+                pq.push(head->data);
+                head = head->next;
+            }
+        }
+        return h;
+    }
+};
+
+int main()
+{
+    return 0;
+}
