9defb9897720a6946bc94d892cef3b8b07b99f10
Avinash Agrawal <115834175+Avinash0308@users.noreply.github.com>
2024-12-02 23:13:28 +0530

    Added 02 Dec POTD

--- 02_Dec_KMP_Algorithm_Solution.cpp
+++ 02_Dec_KMP_Algorithm_Solution.cpp
@@ -1,61 +1,61 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+class find
+{
 public:
-vector<int> get_lps(string &pat)
-{
-    int n = pat.size();
-    vector<int> lps(n, 0);
-    int len = 0, i = 1;
-    while (i < n)
+    vector<int> get_lps(string &pat)
     {
-        if (pat[i] == pat[len])
+        int n = pat.size();
+        vector<int> lps(n, 0);
+        int len = 0, i = 1;
+        while (i < n)
         {
-            len++;
-            lps[i] = len;
-            i++;
-        }
-        else
-        {
-            if (len)
-                len = lps[len - 1];
-            else
-                i++;
-        }
-    }
-    return lps;
-}
-vector<int> search(string &pat, string &txt)
-{
-    // code here
-    vector<int> lps = get_lps(pat);
-    vector<int> res;
-    int len = 0, i = 0;
-    int n = pat.size(), m = txt.size();
-    while (i < m)
-    {
-        if (txt[i] == pat[len])
-        {
-            len++;
-            i++;
-            if (len == n)
+            if (pat[i] == pat[len])
             {
-                res.push_back(i - len);
-                len = lps[len - 1];
+                len++;
+                lps[i] = len;
+                i++;
+            }
+            else
+            {
+                if (len)
+                    len = lps[len - 1];
+                else
+                    i++;
             }
         }
-        else
-        {
-            if (len != 0)
-                len = lps[len - 1];
-            else
-                i++;
-        }
+        return lps;
     }
-    return res;
-}
-}
-;
+    vector<int> search(string &pat, string &txt)
+    {
+        // code here
+        vector<int> lps = get_lps(pat);
+        vector<int> res;
+        int len = 0, i = 0;
+        int n = pat.size(), m = txt.size();
+        while (i < m)
+        {
+            if (txt[i] == pat[len])
+            {
+                len++;
+                i++;
+                if (len == n)
+                {
+                    res.push_back(i - len);
+                    len = lps[len - 1];
+                }
+            }
+            else
+            {
+                if (len != 0)
+                    len = lps[len - 1];
+                else
+                    i++;
+            }
+        }
+        return res;
+    }
+};
 
 int main()
 {
