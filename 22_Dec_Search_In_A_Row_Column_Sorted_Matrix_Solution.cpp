214a239441ec718de916b2ddca23a3f7f64642be
Avinash Agrawal <115834175+Avinash0308@users.noreply.github.com>
2024-12-22 22:59:06 +0530

    Added 22 Dec POTD

---
+++ 22_Dec_Search_In_A_Row_Column_Sorted_Matrix_Solution.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    bool matSearch(vector<vector<int>> &mat, int x) {
+        // your code here
+        int i = 0, j = mat[0].size() - 1;
+        while(i<mat.size() && j>=0){
+            if(mat[i][j]==x) return true;
+            else if(mat[i][j] < x) i++;
+            else j--;
+        }
+        return false;
+    }
+};
+int main(){
+    return 0;
+}
